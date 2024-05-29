c24274ad6730a9e49f4f8c8e4225819fce945f0b
sreerag <sreeragtv007@gmail.com>
2024-05-29 15:00:13 +0530

    initial commit

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('',index,name='index'),
+    path('chatroom/<str:pk>/',chat,name='chat')
+]
