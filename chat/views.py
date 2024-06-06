d83c02f00d847baff521c8e706f17f86d84b2747
sreerag <sreeragtv007@gmail.com>
2024-06-06 10:49:00 +0530

    websocket test

--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,9 @@ def index(request):
     room=Room.objects.all()
     context={'room':room}
     return render(request,'index.html',context)
+
+def joinRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    rooms=Room.objects.all()
+    context={'room':room,'rooms':rooms}
+    return render(request,'room.html',context)
