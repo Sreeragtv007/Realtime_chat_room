7d4afd49eb5e46a511ca779741eb3a0f1b14d086
sreerag <sreeragtv007@gmail.com>
2024-06-09 11:07:36 +0530

    room card design changed

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Room(models.Model):
     name=models.TextField()
-    room_img=models.ImageField(upload_to='room_images',blank=True, null=True)
+    room_img=models.ImageField(upload_to='images/',blank=True, null=True)
     
     
     def __str__(self):
